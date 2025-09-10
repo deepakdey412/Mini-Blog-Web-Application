437f711b1d31d69b63a8e32cef185d1319efdd24
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-10 19:40:54 +0530

    Add mini-blog-frontend folder

---
+++ mini-blog-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+export default function app(){
+  return(
+    <div>
+      <h1>
+        This is the first page;
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto illum rem possimus libero error saepe quaerat cupiditate, dolore dolores, repellat perferendis consequuntur deserunt, repellendus iste ipsa vitae dicta nesciunt omnis!
+      </h1>
+    </div>
+  )
+}
--- mini-blog/src/main/java/com/deepak/mini_blog/MiniBlogApplication.java
+++ mini-blog/src/main/java/com/deepak/mini_blog/MiniBlogApplication.java
@@ -2,12 +2,25 @@ package com.deepak.mini_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MiniBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniBlogApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configurerCROS(){
+		return new WebMvcConfigurer() {
+			public  void addCORSMapping(CorsRegistry registry){
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+			}
+		};
 	}
 
 }
