e279d7aa786a2f902ebe611520cd84ea35cd6f20
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-11 10:50:31 +0530

    Update Controller

--- mini-blog/src/main/java/com/deepak/mini_blog/controller/PostController.java
+++ mini-blog/src/main/java/com/deepak/mini_blog/controller/PostController.java
@@ -2,11 +2,14 @@ package com.deepak.mini_blog.controller;
 
 import com.deepak.mini_blog.dto.PostDto;
 import com.deepak.mini_blog.model.Category;
+import com.deepak.mini_blog.model.Post;
 import com.deepak.mini_blog.model.User;
 import com.deepak.mini_blog.service.PostServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -17,6 +20,7 @@ public class PostController {
         this.postService = postService;
     }
 
+    // ===== Create a post =====
     @PostMapping("/users/{userId}/categories/{categoryId}")
     public ResponseEntity<PostDto> createPost(
             @RequestBody PostDto postDto,
@@ -27,7 +31,34 @@ public class PostController {
 
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
     }
+    // ===== Get all posts =====
+    @GetMapping("/posts")
+    public ResponseEntity<List<PostDto>> getAllPosts() {
+        List<PostDto> posts = this.postService.getAllPosts();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
 
-    @PutMapping("/users/{userId}/categories/{categoryId}")
+    // ===== Get a post by ID =====
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable Integer postId) {
+        PostDto postDto = this.postService.getPostById(postId);
+        return new ResponseEntity<>(postDto, HttpStatus.OK);
+    }
+
+
+    // ===== Update a post =====
+    @PutMapping("/{postId}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto,
+                                              @PathVariable Integer postId){
+        PostDto updatedPost = this.postService.updatePost(postDto,postId);
+        return ResponseEntity.ok(updatedPost);
+    }
+
+    // ===== Delete a post =====
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<String> deletePost(@PathVariable Integer postId) {
+        this.postService.deletePost(postId);
+        return new ResponseEntity<>("Post deleted successfully with ID: " + postId, HttpStatus.OK);
+    }
 
 }
