f6bbe919a573e9f1a1f001c5cbd12c538090448e
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-11 09:59:29 +0530

    Add category repository

--- mini-blog/src/main/java/com/deepak/mini_blog/repository/CategoryRepository.java
+++ mini-blog/src/main/java/com/deepak/mini_blog/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.deepak.mini_blog.repository;
 
-public interface CategoryRepository{
+import com.deepak.mini_blog.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category , Integer> {
 }
